331ff5b8abdb005d5b27873c5fb1ebdc6bb41a9e
Ben Foxall <ben.foxall@oxbotica.com>
2020-07-28 13:33:47 +0100

    Apply suggestions from code review
    Great!
    
    Co-authored-by: Ben Foxall <benfoxall@gmail.com>

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 export function useMeme() {
   return {
     src:
-      'https://memegenerator.net/img/instances/84806978/one-does-not-simply-memoize-a-value.jpg',
+      "https://memegenerator.net/img/instances/84806978/one-does-not-simply-memoize-a-value.jpg",
     alt: 'One does not simply memoize a value',
   };
 }
 
-export const shouldI = () => 'YES_YOU_SHOULD';
+export const shouldI = () => "YES_YOU_SHOULD";
