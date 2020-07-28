26df1f94d1674ebb6d7332883daae7d2ae67a8d3
Ben Foxall <ben.foxall@oxbotica.com>
2020-07-28 12:41:06 +0100

    More FUNctional

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
-export const useMeme = () => ({
-  src:
-    'https://memegenerator.net/img/instances/84806978/one-does-not-simply-memoize-a-value.jpg',
-  alt: 'One does not simply memoize a value',
-});
+export function useMeme() {
+  return {
+    src:
+      'https://memegenerator.net/img/instances/84806978/one-does-not-simply-memoize-a-value.jpg',
+    alt: 'One does not simply memoize a value',
+  };
+}
